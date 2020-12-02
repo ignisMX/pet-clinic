0f1aa2a98e26e339375ae5b54408a8f428a9d03b
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2020-12-02 12:24:12 -0600

    initial commit spring framework guru

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/PetService.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.service;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService extends CrudService<Pet, Long>{
+
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/VetService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.service;
+
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService extends CrudService<Vet, Long>{
+}
